18fc9186b6463d43de9a789b5a70dd4968e4fb8e
yryr92 <rin217@naver.com>
2022-03-09 17:14:01 +0900

    220309 bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -28,6 +30,24 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+})
+
+userSchema.pre('save', function(next) {
+    var user = this;
+
+    if(user.isModified('password')) {
+            // 비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                // Store hash in your password DB.
+                if(err) return next(err)
+                user.password = hash;
+                next()
+            });
+        });
     }
 })
 
